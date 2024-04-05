fa8def8413ef123a1f8c19ab7303a2a6b7a7463e
nzcsx <vulpes.wang@gmail.com>
2024-04-05 09:53:17 -0400

    [intersection] updated temperature to 2

--- intersection/intersection_gpt.py
+++ intersection/intersection_gpt.py
@@ -187,7 +187,7 @@ class DrivingGame:
         response = openai.chat.completions.create(
             model='gpt-4',
             messages=prompt,
-            temperature=0.0,
+            temperature=2.0,
             max_tokens=10,
         )
         rspns_text = response.choices[0].message.content
